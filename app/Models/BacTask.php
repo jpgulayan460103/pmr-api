ace3e1f7af116f6d8c477639a0070a9d6c66b381
John Paul Gulayan <jpgulayan@gmail.com>
2021-12-07 09:29:50 +0800

    rename bac pr to back task

--- app/Models/BacTask.php
+++ app/Models/BacTask.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BacPurchaseRequest extends Model
+class BacTask extends Model
 {
     use HasFactory;
 }
--- database/migrations/2021_12_07_012524_create_bac_tasks_table.php
+++ database/migrations/2021_12_07_012524_create_bac_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBacPurchaseRequestsTable extends Migration
+class CreateBacTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateBacPurchaseRequestsTable extends Migration
      */
     public function up()
     {
-        Schema::create('bac_purchase_requests', function (Blueprint $table) {
+        Schema::create('bac_tasks', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('purchase_request_id')->nullable();
-            $table->string('bac_purchase_request_uuid')->nullable();
+            $table->string('bac_task_uuid')->nullable();
             $table->string("preproc_conference")->nullable();
             $table->string("post_of_ib")->nullable();
             $table->string("prebid_conf")->nullable();
@@ -41,6 +41,6 @@ class CreateBacPurchaseRequestsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bac_purchase_requests');
+        Schema::dropIfExists('bac_tasks');
     }
 }
